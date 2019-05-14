9884ac5e44b1c6fb87b4113810b797f1e1376a74
FreckledPapaya <maya.helferty@gmail.com>
2019-05-14 12:24:41 -0700

    Initialize board with grid, create [] helper method

---
+++ board.rb
@@ -0,0 +1,31 @@
+require_relative "piece.rb"
+
+class Board
+
+  def initialize
+    @grid = Array.new(8) {Array.new(8)}
+  end
+
+  def populate_grid
+    
+  end
+
+  def movie_piece(start_pos, end_pos)
+
+  end
+
+
+  #helper methods
+
+  def [](num1, num2)
+    @grid[num1][num2]
+  end
+
+  def valid_pos?(num1,)
+  
+
+  end
+
+  
+
+end
--- piece.rb
+++ piece.rb
@@ -1,3 +1,12 @@
 
 class Piece
+  attr_reader :name
+
+  def initialize
+    @name = "P"
+  end
+
+  def inspect
+    self.name
+  end
 end
