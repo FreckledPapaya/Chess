c49ab995e97c3755d3bef8213b9e5cb59ff8f419
freckledpapaya <maya.helferty@gmail.com>
2019-05-20 19:55:57 -0700

    Correct Slideable#moves to work with Rook#move_dirs, pry tested

---
+++ rook.rb
@@ -0,0 +1,9 @@
+require_relative "piece.rb"
+require_relative "slideable.rb"
+
+class Rook < Piece
+    include Slideable
+    def move_dirs
+        [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    end
+end
--- slideable.rb
+++ slideable.rb
@@ -3,12 +3,18 @@ module Slideable
         options = []
 
         move_dirs.each do |dir|
-            pos_option = pos
+            pos_option = new_pos(pos, dir)
             until !valid_pos?(pos_option)
-                pos_option = [pos_option[0] + dir[0], pos_option[1] + dir[1]]
                 options << pos_option
+                pos_option = new_pos(pos_option, dir)
+                
             end
         end
+
         options
+    end
+
+    def new_pos(start_pt, dir)
+        [start_pt[0] + dir[0], start_pt[1] + dir[1]]
     end
 end
