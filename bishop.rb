c235d4d1d4b3b4ba2b515b5ad2cffdafa571463e
freckledpapaya <maya.helferty@gmail.com>
2019-05-20 20:00:28 -0700

    Add Queen#move_dirs, Bishop#move_dirs, Slideable::HPrizontal and Slideable::Diagonal arrays

---
+++ bishop.rb
@@ -0,0 +1,9 @@
+require_relative "piece.rb"
+require_relative "slideable.rb"
+
+class Bishop < Piece
+    include Slideable
+    def move_dirs
+        [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+    end
+end
