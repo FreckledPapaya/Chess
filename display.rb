72c316b679bd8429fd23da40c4d2ee404ec7bed6
freckledpapaya <maya.helferty@gmail.com>
2019-05-20 17:07:01 -0700

    Add colorize to cursor position in Display#render, still buggy

--- display.rb
+++ display.rb
@@ -1,8 +1,9 @@
 require "colorize"
 require_relative "cursor.rb"
+require_relative "board.rb"
 
 class Display
-    attr_reader :board
+    attr_reader :board, :cursor
 
     def initialize(board)
         @board = board
@@ -20,7 +21,16 @@ class Display
                     this_row << piece.value
                 end
             end
-            puts "#{i} #{this_row.join(" ")}"
+            if cursor.cursor_pos[0] == row
+                col = cursor.cursor_pos[1]
+                puts "#{i} #{this_row[0...col].join(" ")} " + "#{this_row[col]}".red + " #{this_row[col+1..-1].join(" ")}"
+            else
+                puts "#{i} #{this_row.join(" ")}"
+            end
         end
+        true
     end
 end
+
+b = Board.new(Board.populate)
+d = Display.new(b)
